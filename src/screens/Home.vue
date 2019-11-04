c8ac826da49506aedbb4bc8952c5f1b36e2533e6
Dustin Rouillard <dustin@tetra.dev>
2019-11-03 21:36:23 -0800

    Adding hangman style name display. (I got bored... LOL)

--- src/screens/Home.vue
+++ src/screens/Home.vue
@@ -2,10 +2,7 @@
   <div class="main">
     <div class="header">
       <img alt="avatar" class="avatar" src="../assets/avatar.jpg">
-      <div class="info">
-        <h1>Dustin Rouillard</h1>
-        <h3>Fullstack Developer / System Administrator</h3>
-      </div>
+      <Info />
       <div class="button-container">
         <button class="big-button margin-right" @click="showModal('about')">About</button>
         <button class="big-button margin-right" @click="showModal('projects')">Projects</button>
@@ -20,12 +17,14 @@
 <script>
 import Socials from '@/components/items/Socials';
 import Modal from '@/components/Modal';
+import Info from '@/components/modules/Info';
 
 export default {
   name: 'home',
   components: {
     Socials,
-    Modal
+    Modal,
+    Info
   },
   data() {
     return {
