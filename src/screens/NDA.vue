8100807118b728a0718f8258e9605c8b0b863465
Dustin Rouillard <dustin@tetra.dev>
2020-01-22 10:51:19 -0800

    Change stream url

--- src/screens/NDA.vue
+++ src/screens/NDA.vue
@@ -41,7 +41,7 @@ export default {
         controls: true,
         sources: [
           {
-            src: "http://10.10.2.65:9200/live/dustin/index.m3u8",
+            src: "http://nda.dustin.sh/live/dustin/index.m3u8",
             type: "application/x-mpegURL"
           }
         ]
